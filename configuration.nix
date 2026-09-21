475259929e7924c773c30d09bf0e5fd87c087de8
PalinGone-Master <palingone@gmail.com>
2026-09-21 23:46:29 +0200

    v0.31.7 : fausse mise à jour de test (changement de version uniquement)

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,7 @@
   #==============================================================================================================================
   # VERSION — le SEUL endroit à modifier pour publier une nouvelle version (doit correspondre au tag Git vX.Y.Z)
   #==============================================================================================================================
-  palingoneos.version = "0.31.6";
+  palingoneos.version = "0.31.7";
 
   # Activation de Nix Experimental
   nix.settings.experimental-features = [
